7bf1b13551b6d13a0883ff74c08b25c511400dee
Pawel Konior <pawel.konior@gmail.com>
2023-05-06 13:11:21 +0200

    django admin setup for courses

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = models.Module
+
+
+@admin.register(models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subject', 'created_at')
+    list_editable = ('subject',)
+    list_filter = ('created_at', 'subject')
+    search_fields = ('title', 'overview')
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = (ModuleInline,)
